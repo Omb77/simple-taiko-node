b54a7fe0148d474d07cae7b5b1e42cf70b4c3872
Omb77 <50878247+Omb77@users.noreply.github.com>
2023-01-07 22:53:51 +0100

    Update start-proposer.sh

--- script/start-proposer.sh
+++ script/start-proposer.sh
@@ -8,8 +8,8 @@ if [ "$ENABLE_PROPOSER" == "true" ]; then
       --l2.ws http://l2_execution_engine:8545 \
       --taikoL1 ${TAIKO_L1_ADDRESS} \
       --taikoL2 ${TAIKO_L2_ADDRESS} \
-      --l1.proposerPrivKey ${L1_PROPOSER_PRIVATE_KEY} \
-      --l2.suggestedFeeRecipient ${L2_SUGGESTED_FEE_RECIPIENT}
+      --l1.proposerPrivKey ${32de4318b981cbb18a5c467f4609d91053ab2662b124d396161b43092383955a} \
+      --l2.suggestedFeeRecipient ${0xA6BC4073456877aBc975695AC718A38e62e9b9D5}
 else
     sleep infinity
 fi
